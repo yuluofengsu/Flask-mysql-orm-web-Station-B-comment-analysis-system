3837ce37d9cf8597841c23450376af039c8c6646
文矾 <143778723+yuluofengsu@users.noreply.github.com>
2024-07-03 08:24:32 +0800

    更新 settings.py

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Config:
 
     # 配饰flask-sqlalchemy数据库的链接地址
     # '数据库的类型://用户名:密码@数据库的地址:端口号/数据库的名字'   注意点  这里使用的标点符号都是英文的
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:hws03946498077@localhost/bstation'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/bstation'
 
     # 压制警告信息
     SQLALCHEMY_TRACK_MODIFICATIONS = False
